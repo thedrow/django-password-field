cb6f2e3ab47a3371ced329ca498b6d143d1a37eb
Omer Katz <omer.drow@gmail.com>
2014-09-21 14:45:14 +0300

    Trying to make it work on Python 3.x.

--- password_field/validators.py
+++ password_field/validators.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 from django.utils.translation import ugettext_lazy as _
+from django.core.exceptions import ValidationError
 
 try:
     from cracklib import FascistCheck, VeryFascistCheck
-except ImportError:
-    pass
-else:
-    from django.core.exceptions import ValidationError
 
     def cracklib_fascist_check_password_validator(password):
         try:
@@ -20,12 +17,12 @@ else:
             VeryFascistCheck(password)
         except ValueError as e:
             raise ValidationError(e.message)
+except ImportError:
+    pass
 
 try:
     import zxcvbn
-except ImportError:
-    pass
-else:
+
     class ZXCVBNValidator(object):
         message = _("Passwords must be at least %s characters and of sufficient complexity")
         code = "zxcvbn"
@@ -37,3 +34,5 @@ else:
             res = zxcvbn.password_strength(value)
             if res.get('entropy') < self.password_minimum_entropy:
                 raise ValidationError(self.message % _("Password is too weak"), code=self.code)
+except ImportError:
+    pass
